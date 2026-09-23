94c1e1a4e7833bf1f34307a1cfb11a0355ff09f4
chernousovanton84-sketch <_@example.com>
2026-09-23 08:18:00 +0300

    Add API: serializers, views, urls for ads

---
+++ ads/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import AdListCreateView, AdDetailView
+
+
+urlpatterns = [
+    path('ads/', AdListCreateView.as_view(), name='ad-list-create'),
+    path('ads/<int:pk>/', AdDetailView.as_view(), name='ad-detail'),
+]
--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Ad
+from .serializers import AdSerializer
 
-# Create your views here.
+
+class AdListCreateView(generics.ListCreateAPIView):
+    serializer_class = AdSerializer
+
+    def get_queryset(self):
+        queryset = Ad.objects.all()
+
+        status_filter = self.request.query_params.get('status', 'published')
+        queryset = queryset.filter(status=status_filter)
+
+        search = self.request.query_params.get('search')
+        if search:
+            queryset = queryset.filter(title__icontains=search)
+
+        return queryset.order_by('-created_at', '-id')
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AdDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
